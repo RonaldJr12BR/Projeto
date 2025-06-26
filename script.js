ee9aa0af33e6bb9d1767e3647ad4364bf10f1e78
Ronald Jr <ronald.jro1@icloud.com>
2025-06-26 16:00:38 -0300

    atualizando link GitHub

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// 
+//
 
 function toggleMode() {
-  const html = document.documentElement
+  const html = document.documentElement;
 
   /*if(html.classList.contains('light')) {
     html.classList.remove('light')
@@ -10,11 +10,9 @@ function toggleMode() {
   }  
     da pra facilitar o codigo de cima fazendo o codigo de baixo*/
 
-    html.classList.toggle("light")
+  html.classList.toggle("light");
 
-    
-    
-    /*Pegar a tag img
+  /*Pegar a tag img
     const img = document.querySelector("#profile img")
 
     /* substituir a imagem
@@ -26,44 +24,43 @@ function toggleMode() {
       se tiver sem light mode, manter a imagem normal
       img.setAttribute("src", "./nome da pasta/nome q a foto esta salva")
     } */
-
-  
 }
 
-function criaBotoes(){
+function criaBotoes() {
   // Array de botões
   var links = [
-    {label: 'Inscrever-se no NLW', href: '#'},
-    {label: 'Baixar meu e-book', href: '#'},
-    {label: 'Ver meu portfólio', href: 'https://github.com/RonaldJr12BR'},
-    {label: 'Carros esportivos', href: 'https://carros-esportivos-zeta.vercel.app/'},
-    {label: 'Conheça o explorer', href: '#'},
-    {label: "Google", href:"https://google.com.br"},
-    {label: "Youtube", href:"https://youtube.com"},
-    {label: "Rockeseat", href:"https://app.rocketseat.com.br/"},
-
-  ]
+    { label: "Inscrever-se no NLW", href: "#" },
+    { label: "Baixar meu e-book", href: "#" },
+    { label: "Ver meu portfólio", href: "https://github.com/RonaldWB-jr" },
+    {
+      label: "Carros esportivos",
+      href: "https://carros-esportivos-zeta.vercel.app/",
+    },
+    { label: "Conheça o explorer", href: "#" },
+    { label: "Google", href: "https://google.com.br" },
+    { label: "Youtube", href: "https://youtube.com" },
+    { label: "Rockeseat", href: "https://app.rocketseat.com.br/" },
+  ];
 
   // Pego a UL cujo ID é "botoes"
-  const ul_botoes = document.getElementById('botoes')
+  const ul_botoes = document.getElementById("botoes");
 
   // Faço um laço em todos os botões do array
   for (var i = 0; i < links.length; i++) {
-
     // Crio uma tag "LI" => [<li></li>]
     const new_li = document.createElement("li");
 
     // Crio uma tag "A" => [<a></a>]
-    const new_a = document.createElement("a")
+    const new_a = document.createElement("a");
 
     // Adiciono o HREF na tag A que eu criei
-    new_a.href = links[i].href
+    new_a.href = links[i].href;
 
     // Adiciona o texto do botãon na tag A que eu criei
-    new_a.innerHTML = links[i].label
+    new_a.innerHTML = links[i].label;
 
     // Adiciona a tag "A" dentro do "LI" que criei
-    new_li.append(new_a)
+    new_li.append(new_a);
 
     // E ficou assim
     // <li>
@@ -71,12 +68,10 @@ function criaBotoes(){
     // </li>
 
     // Adiciona o "LI" dentro da "UL" de botoes
-    ul_botoes.append(new_li)
+    ul_botoes.append(new_li);
   }
 }
 
-
-window.onload = function() {
-  criaBotoes()
+window.onload = function () {
+  criaBotoes();
 };
-
